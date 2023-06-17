98dc4c29319f6812535debdc17df9ebbcaa18e5d
syafiqsyauqi <syafiqsyauqi476@gmail.com>
2023-06-17 18:56:06 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if0157/review2/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if0157.review2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
